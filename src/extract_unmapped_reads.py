b96c5f4720fdd006bd08bdd1fd551a67c3289720
tjiang <tjiang@hit.edu.cn>
2018-07-09 10:14:38 +0800

    extract Unmapped reads

---
+++ src/extract_unmapped_reads.py
@@ -0,0 +1,44 @@
+import pysam, sys
+import time
+
+def load_alignment(path):
+	# print path
+	samfile = pysam.AlignmentFile(path)
+	for read in samfile.fetch():
+		# print read.query_name, read.flag
+		# print read.query_sequence
+		# print read.qual
+		# break
+		if read.flag == 4:
+			print read.query_name
+			print read.query_sequence
+			print "+"
+			print read.qual
+
+def parse_alignment(path):
+	file = open(path, 'r')
+	for line in file:
+		seq = line.strip('\n').split('\t')
+		if seq[0][0] == '@':
+			continue
+
+		# print seq[0]		
+
+		read_name = seq[0]
+		read_flag = seq[1]
+		read_seq = seq[9]
+		read_qual = seq[10]
+
+		if read_flag == '4':
+			print read_name
+			print read_seq
+			print '+'
+			print read_qual
+			# break
+	file.close()
+
+if __name__ == '__main__':
+	# starttime = time.time()
+	# load_alignment(sys.argv[1])
+	parse_alignment(sys.argv[1])
+	# print("[INFO]: Finished in %0.2f seconds."%(time.time() - starttime))
---
+++ src/parsing_orphans.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+''' 
+ * All rights Reserved, Designed By HIT-Bioinformatics   
+ * @Title:  process.py
+ * @Package: 
+ * @Description: Control the deNSF pipeline
+ * @author: tjiang
+ * @date: June 11 2018
+ * @version V1.0.1     
+'''
+
+import argparse
+import sys
+import pysam
